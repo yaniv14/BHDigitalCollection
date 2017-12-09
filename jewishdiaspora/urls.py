c5a2d533780138dca4ae98969d5c548f2eefb264
Yaniv Mirel <yanivmirel@gmail.com>
2017-12-09 23:13:04 +0200

    Update urls pattern for Django 2

--- jewishdiaspora/urls.py
+++ jewishdiaspora/urls.py
@@ -1,21 +1,20 @@
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import path
 from django.views.i18n import JavaScriptCatalog
-from django.views.static import serve
 
 from artifacts import views
 
 urlpatterns = [
-    url(r'^$', views.HomeView.as_view(), name='home'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('authtools.urls')),
-    url(r'^jsi18n/$', JavaScriptCatalog.as_view(packages=['artifacts']), name='javascript-catalog'),
-    url(r'^i18n/', include('django.conf.urls.i18n')),
+    path('', views.HomeView.as_view(), name='home'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('authtools.urls')),
+    path('jsi18n/', JavaScriptCatalog.as_view(packages=['artifacts']), name='javascript-catalog'),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += [
-        url(r'^uploads/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-        url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
-    ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
