f3572b32560006070e91c0f7b21835b5162c97e1
ricky <ricky.shimon@gmail.com>
2017-12-14 14:46:40 +0200

    Update urls for donors registration pages

--- artifacts/urls.py
+++ artifacts/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('list/', views.ArtifactListView.as_view(), name='list'),
     path('<int:pk>/', views.ArtifactDetailView.as_view(), name='detail'),
     path('create/', views.ArtifactCreateView.as_view(), name='create'),
+    path('personal_info/', views.PersonalInformationRegistrationPage.as_view(), name='PersonalInformationRegistrationPage'),
+    path('artifact_info/', views.ArtifactInformationRegistrationPage.as_view(), name='ArtifactInformationRegistrationPage'),
+    path('artifact_images/', views.ArtifactsImagesRegistrationPage.as_view(), name='ArtifactsImagesRegistrationPage'),
     path('image/create/', views.ArtifactImageCreateView.as_view(), name='image_create'),
 ]
