339541c2a42bf245e3de830c09562c83dd312ddb
Sabi Reuven <sabireuven@gmail.com>
2017-12-27 03:13:16 +0200

    django-phonenumber-field requirements are filled

--- jewishdiaspora/settings.py
+++ jewishdiaspora/settings.py
@@ -16,6 +16,8 @@ from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+PHONENUMBER_DEFAULT_REGION="IL"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
     'taggit', # todo: safely remove later.
     'django_countries',
     'django_extensions',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
