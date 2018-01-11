474ed534ecd952f448b775b0bf2f1b1fc2ee2ca7
Yaniv Mirel <yanivmirel@gmail.com>
2018-01-11 22:12:19 +0200

    Make items clickable. Cleanups.

--- artifacts/static/js/jewishdiaspora.js
+++ artifacts/static/js/jewishdiaspora.js
@@ -1,20 +1,3 @@
 $(function () {
-    $('.top-links .dropdown').hover(function () {
-        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(100);
-    }, function () {
-        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(100);
-    });
-    // Switch languages
-    $('#lang-switcher a').click(function (e) {
-        e.preventDefault();
-        $('#id_language').val($(this).data('lang'));
-        $('#lang-form').submit();
-    });
 
-    var cards = $.getElementsByClassName("card-columns artifact-set")[0]
-    console.log(cards.length);
-    for (var i = 0; i < cards.length; ++i) {
-    var item = cards[i];
-    console.log(item.innerHTML);
-  }
 });
